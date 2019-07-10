69f0a4b5f4ad1c7b836bbc704812a616b340b350
Jimmyalpha <37843153+Jimmyalpha@users.noreply.github.com>
2019-07-10 10:55:46 +0800

    Update interpreter.h

--- hrm_v2/interpreter.h
+++ hrm_v2/interpreter.h
@@ -1,11 +1,20 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
-
+#include<QVector>
+#include<QString>
+#include "human.h"
 
 class Interpreter
 {
-public:
+public: 
     Interpreter();
+    Interpreter( QVector<QString> *s,Human* human):sv(s),character(human){}
+
+    QVector<QString> *sv;
+    Human *character;
+    int currentcode; //current code in sv to interprete
+    void Interpreter_All();
+    void Interpreter_Step();
 };
 
 #endif // INTERPRETER_H
