0e8848bff2296773b0a42bf2e209227e0496772c
Jimmyalpha <37843153+Jimmyalpha@users.noreply.github.com>
2019-07-10 10:54:13 +0800

    Update human.h
    更新human.h

--- hrm_v2/human.h
+++ hrm_v2/human.h
@@ -4,11 +4,27 @@
 #include <QMainWindow>
 #include <QObject>
 #include <QWidget>
-
+#include <cstring>
+#include<QString>
+using namespace std;
 class Human
 {
+private:
+    int  posx,posy;
+    bool Carry;
+    QString content;
+
 public:
-    Human();
+    Human():posx(0),posy(0),Carry(false),content(""){}
+
+    void setpos(int x,int y);
+    void setflag(bool ok);
+    void setcontent(QString s);
+    QString getcontent();
+    bool isCarry();
+    int  getx();
+    int  gety();
 };
 
 #endif // HUMAN_H
+
