58def17c7b53a34f7f3381eb91747f47a89eff8e
Jimmyalpha <3170102983@zju.edu.cn>
2019-07-11 09:37:03 +0800

    修改了interpreter，person和common中的部分信息

--- hrm/viewmodel/interpreter.cpp
+++ hrm/viewmodel/interpreter.cpp
@@ -1,56 +1,107 @@
 #include "interpreter.h"
 
 void Interpreter::Interpreter_Step(){
-    QString s = sv->at(currentcode);
-    QStringList word = s.split(" ");
-    currentcode++;
-    if ( word.at(0)=="inbox" ){
 
+    QString s = stringVector.at(state.command_ptr);
+    QStringList word = s.split(" ");
+    state.command_ptr++;
+    if ( word.at(0)=="inbox" ){
+        state.p->setpos(left);
+        state.p->setcarry(true);
+        state.p->setcontent(state.in_box[state.pin++]);
     }
     else if (word.at(0)=="outbox"){
-
+        state.p->setpos(right);
+        if (state.p->getcarry()){
+            state.out_box[state.pout++] = state.p->getcontent();
+            state.p->setcarry(false);
+        }
     }
     else if (word.at(0)=="copyto") {
-
+        int temp = word.at(1).toInt();
+        state.p->setpos(mid);
+        if (state.p->getcarry()){
+            state.copybox_item[temp] = state.p->getcontent();
+            state.copybox_flag[temp] = true;
+        }
     }
     else if (word.at(0)=="copyfrom") {
-
+        int temp = word.at(1).toInt();
+        state.p->setpos(mid);
+        if (state.copybox_flag[temp]){
+           state.p->setcontent(state.copybox_item[temp]);
+           state.p->setcarry(true);
+        }
     }
     else if (word.at(0)=="add") {
-        QString temp = character->getcontent();
-        int t = temp.toInt();
-        int incre = word.at(1).toInt();
-        character->setcontent(QString::number(t+incre));
+        int temp = word.at(1).toInt();
+        int n1 = state.p->getcontent();
+        int n2 = 0;
+        if (state.copybox_flag[temp])
+            n2 = state.copybox_item[temp];
+        state.p->setpos(mid);
+        if (state.p->getcarry())
+            state.p->setcontent(n1+n2);
     }
     else if (word.at(0)=="sub") {
-        QString temp = character->getcontent();
-        int t = temp.toInt();
-        int decre = word.at(1).toInt();
-        character->setcontent(QString::number(t-decre));
+        int temp = word.at(1).toInt();
+        int n1 = state.p->getcontent();
+        int n2 = 0;
+        if (state.copybox_flag[temp])
+            n2 = state.copybox_item[temp];
+        state.p->setpos(mid);
+        if (state.p->getcarry())
+            state.p->setcontent(n1-n2);
     }
     else if (word.at(0)=="jump") {
         if (word.at(1)=="if"){
             if (word.at(2)=="zero"){
-                if (character->getcontent().toInt()==0){
-                    currentcode = word.at(3).toInt();
+                if (state.p->getcontent() == 0){
+                    state.command_ptr = word.at(3).toInt();
                 }
             }
             else if (word.at(2)=="negative"){
-                if (character->getcontent().toInt()<0){
-                    currentcode = word.at(3).toInt();
+                if (state.p->getcontent() < 0){
+                    state.command_ptr = word.at(3).toInt();
                 }
             }
         }
         else{
-            currentcode = word.at(1).toInt();
+            state.command_ptr = word.at(1).toInt();
         }
     }
 
+    move.push_back(state);
 }
 
 void Interpreter::Interpreter_All(){
-      QVector<QString>::iterator i;
-    for (i=sv->begin();i!=sv->end();i++){
+    QVector<QString>::iterator i;
+    QVector<int>::iterator  j;
+    int k;
+    ifsuccess = false;
+    for (k=0,j = inbox.begin();j!=inbox.end();k++,j++ ){
+        state.in_box[k] = inbox.at(k);
+    }
+
+    for (k=0,j = copybox.begin();j!=copybox.end();k++,j++ ){
+        state.copybox_item[k] = copybox.at(k);
+        if (state.copybox_item[k]!=-1){
+            state.copybox_flag[k] = true;
+        }
+    }
+
+    for ( i=stringVector.begin();i!=stringVector.end();i++){
           Interpreter_Step();
     }
+
+    if (state.pout+1 == outbox.size()){
+        for ( k = 0; k < state.pout; k++ ){
+            if (state.out_box[k] != outbox.at(k))
+                break;
+        }
+        if ( k == state.pout ){
+              ifsuccess = true;
+        }
+    }
+
 }
